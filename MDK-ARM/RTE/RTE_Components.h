ad8c1cc7b204cfe5797aa379388d8fc0d5ea2227
kekesun <keke.sun@iCareTek.com>
2016-04-13 09:28:51 +0800

    1、创建

---
+++ MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Canon' 
+ * Target:  'Canon' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
